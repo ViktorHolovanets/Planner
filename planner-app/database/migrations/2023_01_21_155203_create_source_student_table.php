f1e56e2ab3b70d6c1fcd3baba6c83a1500d70c42
Viktor <victorgolova@gmail.com>
2023-01-21 18:49:02 +0200

    model migrate

---
+++ planner-app/database/migrations/2023_01_21_155203_create_source_student_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('source_student', function (Blueprint $table) {
+            $table->unsignedBigInteger('source_id');
+            $table->unsignedBigInteger('student_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('source_student');
+    }
+};
